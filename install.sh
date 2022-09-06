3f15e005092c0dc2c048415a656933f64cf70cdb
Giovanni Prisco <gprisco01@gmail.com>
2022-09-06 22:26:47 +0200

    Added scripts

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+git clone https://github.com/decanto-microservices/decanto-food-service.git
+git clone https://github.com/decanto-microservices/decanto-winefamily-service.git
+git clone https://github.com/decanto-microservices/decanto-zone-service.git
+git clone https://github.com/decanto-microservices/decanto-pairing-service.git
